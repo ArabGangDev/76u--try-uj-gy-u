8abad57f3bced1b74f1b13d39360fbe06c19cd9f
ArabGangDev <41093102+ArabGangDev@users.noreply.github.com>
2020-01-21 23:06:49 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
     client.user.setActivity('BH TEAM | Beta', { type: 'WATCHING' });
-  console.log('Im Ready');
+  console.log('Im Ready v1');
 });
 
 var Prefix = ".";
